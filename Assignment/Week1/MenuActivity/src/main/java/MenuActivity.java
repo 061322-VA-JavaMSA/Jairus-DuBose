0206173df6a0afd551012ad2081015a51af63ec1
JaiDuB75 <50184108+JaiDuB75@users.noreply.github.com>
2022-06-16 10:59:20 -0400

    Updated Code in MenuActivity. This code will run but there is a bug in the random number generator. Also I need to implement the do-while loop

--- Assignment/Week1/MenuActivity/src/main/java/MenuActivity.java
+++ Assignment/Week1/MenuActivity/src/main/java/MenuActivity.java
@@ -1,28 +1,48 @@
+import java.util.Random;
 import java.util.Scanner; 
 
 public class MenuActivity {
 	static Scanner scan = new Scanner(System.in);
+	static String name; 
 	public static void main(String[] args) {
 		//Scanner scan = new Scanner(System.in);
 		
 		System.out.println("Hi, Please enter your name: ");
-		String name = scan.nextLine();
+		//String name = scan.nextLine();
+		name = scan.nextLine();
 		
 		System.out.println("You entered: " + name);
+		
+		menuMethod(); 
 		
 		//scan.close();
 	}
 	
 	public static void menuMethod() 
 	{
-		int case = scan;
+		System.out.println("Select 1 for Random Number, 2 for Name Reversal, or 3 for to Exit");
+		int selection = scan.nextInt();
 		
-		switch(case) 
+		switch(selection) 
 		{
 		case 1:
-			
+			Random randomNumber = new Random();
+			System.out.println(randomNumber);
 			break; 
 		case 2: 
+			String reversedName = ""; 
+			
+			char[] letters = name.toCharArray(); 
+			
+			for(int i = letters.length-1; i >= 0 ;i--) {
+				reversedName = reversedName + letters[i];
+			}
+			
+			System.out.println("This is your name reversed: " + reversedName);
+			break;
+		
+		case 3:
+			System.out.println("Your are leaving the program");
 			
 			break;
 		
@@ -30,7 +50,6 @@ public class MenuActivity {
 			System.out.println("Something went wrong"); 
 		
 		}
-		scan.close();
+	scan.close();
 	}
-
 }
