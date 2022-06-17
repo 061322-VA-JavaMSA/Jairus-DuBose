b266c091df7fb6486ac13a6da13e28a34f07f9e5
JaiDuB75 <50184108+JaiDuB75@users.noreply.github.com>
2022-06-17 16:46:54 -0400

    Add class notes directory, please ignore. Add Assignment CalTest, Currently not working. Will continue to work on it

---
+++ Assignment/Week1/TestingActivity/src/main/java/com/revature/CalTest.java
@@ -0,0 +1,32 @@
+package com.revature;
+
+public class CalTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int add(int a, int b) {
+		
+		if(a == 8 && b == 5) {
+		
+		}
+		return a+b;
+		
+	}
+	
+	public int subtract(int a, int b) {
+		
+		if(a == 2 && b == 1) {
+			
+		}
+		
+		return a-b; 
+	}
+	
+	public int sumOfAnArraay(int[] intArr) {
+		return 0; 
+	}
+
+}
