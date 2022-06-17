4b391a155a03e94f7fc443f4709d5f42ff8f5bbd
JaiDuB75 <50184108+JaiDuB75@users.noreply.github.com>
2022-06-17 17:19:02 -0400

    Added some logic to my test, still struggling with it.

--- Assignment/Week1/TestingActivity/src/test/java/com/revature/CalculatorTest.java
+++ Assignment/Week1/TestingActivity/src/test/java/com/revature/CalculatorTest.java
@@ -2,13 +2,46 @@ package com.revature;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorTest {
 
+	private static CalTest sut; 
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception { 
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		sut = new CalTest();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("Tear down behavior.");
+	}
+
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		int expected = 3; 
+		int actual = sut.add(1,2);
+		
+		assertEquals(expected, actual);
+				
 	}
+	
+/*	@Test
+	public void test2() {
+		
+	} */ 
 
 }
