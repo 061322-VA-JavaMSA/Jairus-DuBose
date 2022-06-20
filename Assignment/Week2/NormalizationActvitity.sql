dc2ed3323422bf0acaf883006c3891328e3809c2
JaiDuB75 <50184108+JaiDuB75@users.noreply.github.com>
2022-06-20 18:33:11 -0400

    Added Week2 folder into directory also added the NormalizationActivity

---
+++ Assignment/Week2/NormalizationActvitity.sql
@@ -0,0 +1,49 @@
+--This is my single line commit
+
+/*
+ * This is a multi line commit
+ */
+
+--create database mydb; 
+
+--create schema task_manager;
+--drop schema task_manager; 
+/*
+create table users(
+id serial primary key,
+username varchar(30),
+password varchar(30)
+);*/
+
+/*
+ * EmployeeId
+ * SalesPerson
+ * SalesOffice
+ * Age
+ * DoB
+ * Customer1
+ * Customer2
+ * Customer3
+*/
+
+--Seperated the employeees Into their own table
+create empolyees(
+	EmolyeeId serial primary key,
+	SalesPersonFirstName Varchar(50),
+	SalesPersonLastName Varchar(50),
+	DoB Date
+);
+
+--Seperated the customers into their own table
+create customers(
+	CustomerID serial primary key,
+	CustomerFirstName Varchar(50),
+	CustomerLastName Varchar(50),
+	DoB Date
+);
+
+--In this table I put the Sales Office location because both the 
+--employee and customer can/probaby in the same building
+create employee_customer(
+	SalesOfficeLocation INTEGER
+);
--- Projects/Project0/src/main/java/com/revature/Users.java
+++ Projects/Project0/src/main/java/com/revature/Users.java
@@ -1,30 +1,39 @@
 package com.revature;
 
+import java.util.Scanner;
+
 public class Users  implements UsersInterface {
 	
-	static String userType;
+	static Scanner userInput = new Scanner(System.in); 
+	
+	static String userName;
 	static String userPassword; 
 	static int userGold; 
 	
 	public Users(String userType, String userPassword) 
 	{
-		Users.userType = userType;
-		Users.userPassword = userPassword; 
+		System.out.println("Please Enter Your Name/Username: "); 
+		String userName = userInput.nextLine();
+		//Users.userName = userType;
 		
-		AccountLogin(userType, userPassword); 
+		System.out.println("Please Enter Your Password: ");
+		String userPassword1 = userInput.nextLine();
+		//Users.userPassword = userPassword; 
+		
+		AccountLogin(userType, userPassword1); 
 		//System.out.println("This worked " + userType);
 	}
 	
 	public static void AccountLogin(String userType, String userPassword) {
 		
 		if(userType.equals("Jairus") && userPassword.equals("JaiDuB75")){
-			System.out.println("Welcome " + userType + " You are a manager");
+			System.out.println("Welcome " + userName + " You are a manager");
 		}
 		else if (userType.equals("John")) {
-			System.out.println("Welcome " + userType + " You are an employee");
+			System.out.println("Welcome " + userName + " You are an employee");
 		}
 		else {
-			System.out.println("Welcome " + userType + " You are customer"); 
+			System.out.println("Welcome " + userName + " You are customer"); 
 		}
 		
 	}
