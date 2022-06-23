aa9b07eaae1eda99f4aa211e70a44d2149946bb9
JaiDuB75 <50184108+JaiDuB75@users.noreply.github.com>
2022-06-23 17:50:49 -0400

    Established a connection to my database and returned data from the database

--- Projects/Project0/src/main/java/com/revature/Users.java
+++ Projects/Project0/src/main/java/com/revature/Users.java
@@ -10,17 +10,23 @@ public class Users  implements UsersInterface {
 	static String userPassword; 
 	static int userGold; 
 	
+	private int id;
+	private String username;
+	private String password;
+	private String usertype;
+	
 	public Users() 
 	{
-		System.out.println("Please Enter Your Name/Username: "); 
-		String userName = userInput.nextLine();
+		super(); 
+		//System.out.println("Please Enter Your Name/Username: "); 
+		//String userName = userInput.nextLine();
 		
 		
-		System.out.println("Please Enter Your Password: ");
-		String userPassword = userInput.nextLine();
+		//System.out.println("Please Enter Your Password: ");
+		//String userPassword = userInput.nextLine();
 		 
 		
-		NewUserLogin(userName, userPassword);  
+		//NewUserLogin(userName, userPassword);  
 		
 	}
 	
@@ -58,4 +64,83 @@ public class Users  implements UsersInterface {
 		// TODO Auto-generated method stub
 		
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsertype() {
+		return usertype;
+	}
+
+	public void setUsertype(String usertype) {
+		this.usertype = usertype;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		result = prime * result + ((usertype == null) ? 0 : usertype.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Users other = (Users) obj;
+		if (id != other.id)
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		if (usertype == null) {
+			if (other.usertype != null)
+				return false;
+		} else if (!usertype.equals(other.usertype))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Users [id=" + id + ", username=" + username + ", password=" + password + ", usertype=" + usertype + "]";
+	}
 }
+
+
--- Projects/Project0/src/main/java/com/revature/database/DatabaseConnection.java
+++ Projects/Project0/src/main/java/com/revature/database/DatabaseConnection.java
@@ -2,7 +2,11 @@ package com.revature.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+import com.revature.*;
 
 public class DatabaseConnection {
 
@@ -12,16 +16,37 @@ public class DatabaseConnection {
 		String username = "";
 		String password = ""; 
 		
+		Scanner scan = new Scanner(System.in);
+		int input = Integer.parseInt(scan.nextLine());
+		Users u = null; 
+		
 		try {
 			
 			Connection databaseConnection = DriverManager.getConnection(url, username, password);
 			System.out.println(databaseConnection.getMetaData().getDriverName());
 			System.out.println("A connection was established");
 			
+			String sql = "select * from users where users.id =" + input + ";";
+			Connection databaseConnection1 = DriverManager.getConnection(url,username,password);
+			Statement sqlStatement = databaseConnection1.createStatement();
+			ResultSet resultOutput = sqlStatement.executeQuery(sql);
+			while(resultOutput.next()) {
+				u = new Users();
+				u.setId(resultOutput.getInt(input));
+				u.setUsername(resultOutput.getString("username"));
+				u.setPassword(resultOutput.getString("password"));
+				u.setUsertype(resultOutput.getString("usertype"));
+			}
+			
+			databaseConnection.close();
+			databaseConnection1.close();
+			
 		}catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		System.out.println(u);
 		
 	}
 		
